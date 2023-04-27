73b5d179047fa6bdf37d7798c239840814919142
SkySourced <luka.gn.scott@gmail.com>
2023-04-28 09:38:34 +1200

    Added cell updating and rendering

--- Cell.java
+++ Cell.java
@@ -2,9 +2,46 @@ public class Cell {
     private int x;
     private int y;
     public boolean state;
+    public Game game;
+    public Cell(int x, int y, Game game) {
+        this.x = x;
+        this.y = y;
+        this.state = false;
+        this.game = game;
+    }
+    public void update() {
+        /*
+         * 1. Any live cell with fewer than two live neighbours dies, as if caused by under-population.
+         * 2. Any live cell with two or three live neighbours lives on to the next generation.
+         * 3. Any live cell with more than three live neighbours dies, as if by overcrowding.
+         * 4. Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction.
+         */
+        int count = 0; // Number of live neighbors
 
+        if(x > 0 && y > 0 && game.cells[x-1][y-1].state) count++; // top left
+        if(x > 0 && game.cells[x-1][y].state) count++; // left
+        if(x > 0 && y < Game.HEIGHT-1 && game.cells[x-1][y+1].state) count++; // bottom left
+        if(y > 0 && game.cells[x][y-1].state) count++; // top
+        if(y < Game.HEIGHT-1 && game.cells[x][y+1].state) count++; // bottom
+        if(x < Game.WIDTH-1 && y > 0 && game.cells[x+1][y-1].state) count++; // top right
+        if(x < Game.WIDTH-1 && game.cells[x+1][y].state) count++; // right
+        if(x < Game.WIDTH-1 && y < Game.HEIGHT-1 && game.cells[x+1][y+1].state) count++; // bottom right
 
-    public Cell(){
-
+        if(state) { // Cell is alive
+            if (count < 2 || count > 3) { // Cell death (case 1 and 3)
+                state = false;
+            } else {
+                state = true; // Cell lives on (case 2)
+            }
+        } else { // Cell is dead
+            if(count == 3) state = true; // Cell reproduction (case 4)
+        }
+    }
+    public char render(){
+        if(state) {
+            return "■";
+        } else {
+            return "□";
+        }
     }
 }
--- Game.java
+++ Game.java
@@ -1,19 +1,40 @@
-public class Game {
-    public static final int WIDTH = 10;
-    public static final int HEIGHT = 10;
+import java.util.Timer;
+import java.util.Scanner;
 
-    private Cell[][] cells;
+    public class Game {
+    public static final int WIDTH = 50; // Width of the grid
+    public static final int HEIGHT = 50; // Height of the grid
+    public Cell[][] cells; // 2D array of cells
+    private int rate; // Rate of updates per second
+    private static Keyboard kb; // Keyboard input
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        kb = new Scanner(System.in);
         gridInit();
     }
 
-    private static void gridInit(){
+    private static void gridInit() {
         this.cells = new Cell[WIDTH][HEIGHT];
-        for(int i = 0; i < WIDTH; i++){
-            for(int j = 0; j < HEIGHT; j++){
-                cells[i][j] = new Cell();
+        for (int i = 0; i < WIDTH; i++) {
+            for (int j = 0; j < HEIGHT; j++) {
+                cells[i][j] = new Cell(i, j, this);
             }
+        }
+    }
+
+    public void update() {
+        for (Cell[] row : cells) {
+            for (Cell cell : row) {
+                cell.update();
+            }
+        }
+    }
+
+    public void render() {
+        for (Cell[] row : cells) {
+            for (Cell cell : row) {
+                System.out.print(cell.render());
+            }
+            System.out.println();
         }
     }
 }
