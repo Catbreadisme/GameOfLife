ba2231635c2138430fef9f4cfa5afca4dd5c32f9
SkySourced <58579786+SkySourced@users.noreply.github.com>
2023-05-01 15:08:58 +1200

    Added user input and running multiple turns

--- Game.java
+++ Game.java
@@ -32,6 +32,40 @@ public class Game {
                 cells[i][j] = new Cell(i, j, this); // Create a new cell
             }
         }
+        render(); // Render the grid
+        newTurn();
+    }
+    private static void newTurn() {
+        System.out.println("Enter the x coordinate of a cell you would like to toggle, or enter 'next <no. of turns>' to evaluate turns.");
+        String input = kb.nextLine(); // Get the user's input
+        int x;
+        try { // Try to parse the input as an integer
+            Integer.parseInt(input);
+        } catch { // If it fails, check if the input is "next"
+            word = input.split(" ")[0]; // Split the input into words
+            if (word.toLowerCase() == "next") {
+                try {
+                    int turns = Integer.parseInt(input.split(" ")[1]); // Get the number of turns
+                } catch { // No specified turn number, run one turn
+                    runTurns(1);
+                }
+                runTurns(turns); // Run the turns
+            } else { // If it's not "next", then the input is invalid
+                System.out.println("Invalid input. Please enter a number or 'next'.");
+                newTurn(); // Start a new turn
+            }
+        }
+        System.out.println("Now enter the y coordinate of the cell to toggle.");
+        int y = kb.nextInt(); // Get the user's input
+        cells[x][y].state = !cells[x][y].state; // Toggle the cell's state
+        newTurn(); // Start a new turn
+    }
+    public static void runTurns(int turns){
+        for(int i = 0; i < turns; i++){
+            update();
+            render();
+        }
+        newTurn();
     }
     // Update every cell
     public static void update() {
