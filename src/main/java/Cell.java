9f28f91f31d8c7c97763009eb63c1a725db1fd71
SkySourced <luka.gn.scott@gmail.com>
2023-05-08 10:39:29 +1200

    Started debugging #3 (not counting correctly)

--- src/main/java/Cell.java
+++ src/main/java/Cell.java
@@ -15,21 +15,21 @@ public class Cell {
     }
     public void update() { // Update the cell
         /*
-         * 1. Any live cell with fewer than two live neighbours dies, as if caused by under-population.
-         * 2. Any live cell with two or three live neighbours lives on to the next generation.
-         * 3. Any live cell with more than three live neighbours dies, as if by overcrowding.
-         * 4. Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction.
+         * 1) Any live cell with fewer than two live neighbours dies, as if by underpopulation.
+         * 2) Any live cell with two or three live neighbours lives on to the next generation.
+         * 3) Any live cell with more than three live neighbours dies, as if by overpopulation.
+         * 4) Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction.
          */
         int count = 0; // Number of live neighbors
 
-        if(x > 0 && y > 0 && cells[x-1][y-1].state) count++; // top left
-        if(x > 0 && cells[x-1][y].state) count++; // left
-        if(x > 0 && y < Game.HEIGHT-1 && cells[x-1][y+1].state) count++; // bottom left
-        if(y > 0 && cells[x][y-1].state) count++; // top
-        if(y < Game.HEIGHT-1 && cells[x][y+1].state) count++; // bottom
-        if(x < Game.WIDTH-1 && y > 0 && cells[x+1][y-1].state) count++; // top right
-        if(x < Game.WIDTH-1 && cells[x+1][y].state) count++; // right
-        if(x < Game.WIDTH-1 && y < Game.HEIGHT-1 && cells[x+1][y+1].state) count++; // bottom right
+        if(x > 0 && y > 0 && cells[x-1][y-1].state) count++;System.out.println("tl: " + count); // top left
+        if(x > 0 && cells[x-1][y].state) count++;System.out.println("l: " + count); // left
+        if(x > 0 && y < Game.HEIGHT-1 && cells[x-1][y+1].state) count++;System.out.println("bl: " + count); // bottom left
+        if(y > 0 && cells[x][y-1].state) count++;System.out.println("t: " + count); // top
+        if(y < Game.HEIGHT-1 && cells[x][y+1].state) count++;System.out.println("b: " + count); // bottom
+        if(x < Game.WIDTH-1 && y > 0 && cells[x+1][y-1].state) count++;System.out.println("tr: " + count); // top right
+        if(x < Game.WIDTH-1 && cells[x+1][y].state) count++;System.out.println("r: " + count); // right
+        if(x < Game.WIDTH-1 && y < Game.HEIGHT-1 && cells[x+1][y+1].state) count++;System.out.println("br: " + count); // bottom right
 
         if(state) { // Cell is alive
             if (count < 2 || count > 3) { // Cell death (case 1 and 3)
