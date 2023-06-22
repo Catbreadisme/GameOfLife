e89a258ee1dd6a3bbe007f353e7cd579ae07d244
SkySourced <luka.gn.scott@gmail.com>
2023-06-22 15:18:46 +1200

    Added rudimentary automatic mode and repeat checking

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -15,10 +15,9 @@ import java.util.Scanner;
 public class Game {
     // Constants
     public static final int GRID_SIZE = 10; // Size of the grid
-    // Public variables
+    private static final int rate = 1; // Rate of updates per second
+    // Variables
     public static Cell[][] cells; // 2D array of cells
-    // Private variables
-    //private int rate; // Rate of updates per second
     // Library implementations
     public static Scanner kb; // Keyboard input
 
@@ -35,22 +34,38 @@ public class Game {
     private static void newTurn() { // Effectively the main loop
         render(); // Render the grid
         System.out.println("Enter the x coordinate of a cell you would like to toggle, or enter 'next <no. of turns>' to evaluate turns.");
+        System.out.println("You can also enter 'auto <number of repeating states>' to run the simulation automatically, ending when the grid repeats within the specified number of turns. Enter 0 here to run indefinitely.");
         String[] input = kb.nextLine().split(" "); // Get the user's input
         int x = 0; // X coordinate
         int y = 0; // Y coordinate
         int turns; // Number of turns to run
 
-        if(input[0].equalsIgnoreCase("next")) { // If the user enters 'next'
+        if (input[0].equalsIgnoreCase("next")) { // If the user enters 'next'
             try {
                 turns = Integer.parseInt(input[1]); // Get the number of turns
             } catch (NumberFormatException notInt) { // If not an int, ask again
-                System.out.println("Invalid input. Please enter a number or 'next (optional number of turns)'.");
+                System.out.println("Invalid input. Please enter a number of turns to run.");
                 turns = 0;
                 newTurn(); // Start a new turn
             } catch (ArrayIndexOutOfBoundsException oob) { // No specified turn number, run one turn
                 turns = 1;
             }
             runTurns(turns); // Run the turns
+        } else if (input[0].equalsIgnoreCase("auto")) {
+            try {
+                int repeats = Integer.parseInt(input[1]); // Get the number of turns
+                if(repeats < 0){
+                    System.out.println("Invalid input. Please enter a positive number.");
+                } else {
+                    runAuto(repeats);
+                }
+            } catch (NumberFormatException notInt) {
+                System.out.println("Invalid input. Please enter a number of repeating states.");
+                newTurn(); // Start a new turn
+            } catch (ArrayIndexOutOfBoundsException oob) { // No specified turn number, run one turn
+                System.out.println("Invalid input. Please enter a number of repeating states.");
+                newTurn(); // Start a new turn
+            }
         } else {
             x = parseInt(input, x);
             System.out.println("Now enter the y coordinate of the cell to toggle.");
@@ -80,12 +95,47 @@ public class Game {
     public static void runTurns(int turns){
         for(int i = 0; i < turns; i++){
             update();
+            try{
+                Thread.sleep(1000 / rate);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
             if(i+1 < turns) {
                 render();
                 System.out.println("----------"); // buffer between multiple turns
             }
         }
         newTurn();
+    }
+    // Automatic mode
+    public static void runAuto(int states){
+        if(states == 0){
+            while(true){
+                update();
+                render();
+                System.out.println("----------");
+            }
+        } else {
+            Cell[][][] gridStates = new Cell[states][GRID_SIZE][GRID_SIZE];
+            for (int i = states - 1; i > 0; i--) {
+                gridStates[i] = gridStates[i - 1];
+            }
+            gridStates[0] = cells;
+            update();
+            try {
+                Thread.sleep(1000 / rate);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            render();
+            System.out.println("----------");
+            for (int i = 0; i < states; i++) {
+                if (cells == gridStates[i]) {
+                    System.out.println("Grid state repeats after " + states + " turns.");
+                    break;
+                }
+            }
+        }
     }
     // Update every cell
     public static void update() {
